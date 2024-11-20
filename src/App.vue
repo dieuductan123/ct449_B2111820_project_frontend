8529ec6eeda67330a8ec0be5002c99c450181750
Duc Tan <ductandieu456@gmail.com>
2024-11-20 15:03:36 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  
+}
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>Hello, Vue.js!</h1>
+</template>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
